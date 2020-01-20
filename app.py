bf9f74d551be2249890789de448f330359a688bd
Michael O'Farrell <mofarrell1967@gmail.com>
2020-01-20 10:17:22 +0000

    Updated with additional html for projects

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def insert_task():
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))  
 
+
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
     the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
@@ -31,6 +32,55 @@ def edit_task(task_id):
     return render_template('edittask.html', task=the_task,
                            projects=all_projects)
 
+@app.route('/update_task/<task_id>', methods=["POST"])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update( {'_id': ObjectId(task_id)},
+    {
+        'task_name':request.form.get('task_name'),
+        'project_name':request.form.get('project_name'),
+        'task_description': request.form.get('task_description'),
+        'due_date': request.form.get('due_date'),
+        'is_urgent':request.form.get('is_urgent')
+    })
+    return redirect(url_for('get_tasks'))
+
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
+
+@app.route('/get_projects')
+def get_projects():
+    return render_template('projects.html',
+                           projects=mongo.db.projects.find())
+
+@app.route('/delete_project/<project_id>')
+def delete_project(project_id):
+    mongo.db.projects.remove({'_id': ObjectId(project_id)})
+    return redirect(url_for('get_projects'))
+
+@app.route('/edit_project/<project_id>')
+def edit_project(project_id):
+    return render_template('editproject.html',
+    project=mongo.db.projects.find_one({'_id': ObjectId(project_id)}))
+
+@app.route('/update_project/<project_id>', methods=['POST'])
+def update_project(project_id):
+    mongo.db.projects.update(
+        {'_id': ObjectId(project_id)},
+        {'project_name': request.form.get('project_name')})
+    return redirect(url_for('get_projects'))
+
+@app.route('/insert_project', methods=['POST'])
+def insert_project():
+    category_doc = {'project_name': request.form.get('project_name')}
+    mongo.db.projects.insert_one(project_doc)
+    return redirect(url_for('get_projects'))
+
+@app.route('/add_project')
+def add_project():
+    return render_template('addproject.html')
 
 
 
